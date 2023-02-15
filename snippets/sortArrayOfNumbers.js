39286a9d376eff7e8c5e12a3f00cae1d2766b881
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-02-15 18:35:27 -0300

    📝 update: add sort array of numbers snippet

---
+++ snippets/sortArrayOfNumbers.js
@@ -0,0 +1,5 @@
+let numbers = [16, 53, 25, 46, 40, 9, 6, 56, 4, 35, 48, 43, 12, 29, 14, 22, 34, 28, 47, 55, 51, 50, 26, 18, 7, 2, 5]
+
+console.clear()
+
+console.log(numbers.sort(function (a, b) { return a - b }))
