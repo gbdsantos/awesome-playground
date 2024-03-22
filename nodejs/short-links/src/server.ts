a1071c78eed14d2202ebcd390877317d2bf3bcf0
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-03-22 02:44:51 -0300

    ✨ feat(backend): implement endpoint to create shortlink from url

--- nodejs/short-links/src/server.ts
+++ nodejs/short-links/src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import postgres from 'postgres'
 import { z } from 'zod'
+
+import { sql } from './lib/postgres'
 
 const app = fastify()
 
-app.post('/links', (request) => {
+app.post('/links', async (request, reply) => {
   const createLinkSchema = z.object({
     code: z.string().min(3),
     url: z.string().url()
@@ -11,7 +14,31 @@ app.post('/links', (request) => {
 
   const { code, url } = createLinkSchema.parse(request.body)
 
-  return 'Hello World'
+  try {
+    const result = await sql/*sql*/`
+      INSERT INTO short_links(code, original_url)
+      VALUES (${code}, ${url})
+      RETURNING id
+    `
+
+    const link = result[0]
+
+    return reply.status(201).send({ short_link_id: link.id })
+  } catch (error) {
+    if (error instanceof postgres.PostgresError) {
+      if (error.code === '23505') {
+        return reply.status(400).send({
+          message: 'Duplicated code.',
+          status: "error",
+          status_code: 400
+        })
+      }
+    }
+
+    console.error(error)
+
+    return reply.status(500).send({ message: 'Internal server error.'})
+  }
 })
 
 app.listen({
