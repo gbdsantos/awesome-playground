542869a26e75bdd03f9e65f693b89003a73dcf8e
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-03-22 15:47:38 -0300

    ✨ feat(backend): implement get all shortlinks and get one shortlink

--- nodejs/short-links/src/server.ts
+++ nodejs/short-links/src/server.ts
@@ -6,7 +6,43 @@ import { sql } from './lib/postgres'
 
 const app = fastify()
 
-app.post('/links', async (request, reply) => {
+app.get('/:code',  async (request, reply) => {
+  const getLinkSchema = z.object({
+    code: z.string().min(3)
+  })
+
+  const { code } = getLinkSchema.parse(request.params)
+
+  const result = await sql/*sql*/`
+    SELECT id, original_url
+    FROM short_links
+    WHERE short_links.code = ${code}
+  `
+
+  if (result.length === 0) {
+    return reply.status(400).send({
+      message: 'Link not found.',
+      status: "error",
+      status_code: 400
+    })
+  }
+
+  const link = result[0]
+
+  return reply.redirect(301, link.original_url)
+})
+
+app.get('/api/links', async () => {
+  const result = await sql/*sql*/`
+    SELECT *
+    FROM short_links
+    ORDER BY created_at DESC
+  `
+
+  return result
+})
+
+app.post('/api/links', async (request, reply) => {
   const createLinkSchema = z.object({
     code: z.string().min(3),
     url: z.string().url()
